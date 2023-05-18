76be10266392fd85e9a8d7397f444e835f40e8e1
Parvesh Mishra <parvesh225@gmail.com>
2023-05-18 13:06:02 +0530

    fresh project or basic setting done

---
+++ pages/index.jsx
@@ -0,0 +1,14 @@
+import Footer from '@/components/Footer';
+import Header from '@/components/Header';
+
+import styles from '@/styles/Home.module.scss';
+
+export default function Home() {
+    return (
+        <>
+            <Header/>
+            <h1>hello</h1>
+            <Footer/>
+        </>
+    )
+}
