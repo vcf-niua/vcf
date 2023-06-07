7c7546b00dc4bd67f08c325b4262a0bca29c4da2
Abhishek Upperwal <a.upperwal@gmail.com>
2023-06-07 14:16:22 +0530

    mmap india boundary

--- components/Map.jsx
+++ components/Map.jsx
@@ -7,9 +7,11 @@ import styles from '@/styles/Map.module.scss';
 export default function Map(){
     const mapContainer = useRef(null);
     const map = useRef(null);
-    const [lng] = useState(79.378211);
+    const [lng] = useState(82.678211);
     const [lat] = useState(22.287897);
-    const [zoom] = useState(3.5);
+    const [zoom] = useState(3.9);
+    const [indiaBoundary, setIndiaBoundary] = useState();
+    const hoveredStateId = useRef();
 
     useEffect(() => {
         if (map.current) return;
@@ -21,7 +23,83 @@ export default function Map(){
             minZoom: 3
         });
 
+        map.current.on('load', () => {
+            fetch('/maps/india_states.geojson')
+                .then(res => res.json())
+                .then(setIndiaBoundary)
+        })
+
     });
+
+    useEffect(() => {
+        if(indiaBoundary) {
+            map.current.addSource('india-boundary-vcf', {
+                type: 'geojson',
+                data: indiaBoundary,
+                promoteId: 'id'
+            })
+            map.current.addLayer({
+                'id': 'india-boundary-vcf-layer',
+                'type': 'fill',
+                'source': 'india-boundary-vcf',
+                'layout': {},
+                'paint': {
+                    'fill-color': '#badcf1',
+                    'fill-opacity': [
+                        'case',
+                        ['boolean', ['feature-state', 'hover'], false],
+                        0.9,
+                        0.5
+                    ]
+                }
+            }, 'border_india')
+
+            map.current.addLayer({
+                'id': 'india-boundary-vcf-layer-line',
+                'type': 'line',
+                'source': 'india-boundary-vcf',
+                'layout': {},
+                'paint': {
+                    'line-color': '#144665',
+                    'line-width': [
+                        'case',
+                        ['boolean', ['feature-state', 'hover'], false],
+                        3.5,
+                        2
+                    ]
+                }
+            }, 'border_india')
+
+            map.current.on('mousemove', 'india-boundary-vcf-layer', (e) => {
+                if (e.features.length > 0) {
+                    if (hoveredStateId.current) {
+                        map.current.setFeatureState(
+                            { source: 'india-boundary-vcf', id: hoveredStateId.current },
+                            { hover: false }
+                        );
+                    }
+
+                    // console.log(e.features[0])
+                    hoveredStateId.current = e.features[0].id;
+                    map.current.setFeatureState(
+                        { source: 'india-boundary-vcf', id: hoveredStateId.current },
+                        { hover: true }
+                    );
+                }
+            })
+
+            map.current.on('mouseleave', 'india-boundary-vcf-layer', (e) => {
+                console.log('mouseleave')
+                if (hoveredStateId.current) {
+                    map.current.setFeatureState(
+                        { source: 'india-boundary-vcf', id: hoveredStateId.current },
+                        { hover: false }
+                    );
+                }
+                hoveredStateId.current = null;
+            })
+        }
+    }, [indiaBoundary])
 
     return (
         <div className={styles.map_container}>
