433ea27bd20ff1a30e1bf45acabfd4c10f65e913
Parvesh Mishra <parvesh225@gmail.com>
2023-06-09 12:38:31 +0530

    Popup completed on about page

--- services/utils.jsx
+++ services/utils.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+// import React from 'react'
 
-export default function utils() {
-  return (
-    <div>utils</div>
-  )
-}
+// export default function utils(toolDescription) {
+//   if( toolDescription && toolDescription.data.length > 0 ) {
+//     let attributes =  toolDescription.data[0].attributes;
+//         if(attributes && attributes.impact_indicators.data.length > 0) {
+//             return let indicators = attributes.impact_indicators.data[0].attributes ;
+//         }
+//   }else {
+//     return false
+//   }
+// }
