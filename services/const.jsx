bfc07b02780e57e10f8d2e718e2ae98d89b1d0a5
Abhishek Upperwal <a.upperwal@gmail.com>
2023-06-19 17:39:36 +0530

    cms

--- services/const.jsx
+++ services/const.jsx
@@ -1,6 +1,6 @@
 export let API_ENDPOINT_CMS = 'http://localhost:1337'
-API_ENDPOINT_CMS = 'https://vcf.niua.in'
+API_ENDPOINT_CMS = 'https://vcf.niua.in/cms'
 
 if (!process.env.NODE_ENV || process.env.NODE_ENV == 'production') {
-    API_ENDPOINT_CMS = 'https://vcf.niua.in'
+    API_ENDPOINT_CMS = 'https://vcf.niua.in/cms'
 }
